c50edebaeccbd1f68bc1136dfbc48665d76217d1
sagishwartz <sagi71280@gmail.com>
2021-12-22 19:02:22 +0200

    new
    new

---
+++ 1.12/1.12_classword.py
@@ -0,0 +1,50 @@
+import random
+# for n in range(4):
+#     print(random.randint(2,6))
+#
+#
+# die1=random.randint(1,6)
+# die2=random.randint(1,6)
+# print(f'dice are {die1} and {die2}')
+import random
+
+# city=input('provide US city:')
+# print(random.choice(city))
+
+# def printhyphens():
+#     for n in range(20):
+#         print('-', end='')
+#     print()
+#
+# printhyphens()
+
+# def sayHello(name):
+#     print(f'Hello {name}')
+# sayHello('Moshe')
+# sayHello('Haim')
+
+# def timeto12h(time):
+#     if time>12:
+#         time=time-12
+#     if time==0:
+#         time=12
+#     print(time)
+# timeto12h(3)
+# timeto12h(17)
+# timeto12h(20)
+
+# def printperson(name,age,city='TLV'):
+#     print(f'hello{ name}, aged {age}, from {city}')
+# printperson('Itzik',12,'haifa')
+# printperson(age=34,name='Gal')
+
+# def getcircleperimiter(radius):
+#     per=2*3.14*radius
+#     return per
+# print(getcircleperimiter(4))
+
+# def power2(num):
+#     res=num**2
+#     return res
+# print(power2(5))
+# print(power2(10))
